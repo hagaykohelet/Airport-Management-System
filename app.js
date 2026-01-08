6bda6baa6c27fa7016cdec45c2257c16371b7c59
hagay kohelet <hagay2506@gmail.com>
2026-01-08 12:48:29 +0200

    not finish

---
+++ app.js
@@ -0,0 +1,14 @@
+import RegularPassenger from "./classes/passengers/regularPassenger.js";
+import StudentPassenger from "./classes/passengers/studentPassenger.js";
+import Airport from "./classes/airportSystem.js";
+
+const airport = new Airport()
+airport.createFlights()
+airport.addTickets()
+
+const studentPassenger = new StudentPassenger("hagay",12345,10000,"kodkod")
+const regularPassenger = new RegularPassenger("joni",98765,5000,"IDF")
+
+
+regularPassenger.buyTicket()
+
--- classes/airportSystem.js
+++ classes/airportSystem.js
@@ -10,9 +10,46 @@ export default class Airport{
         this.#allFlights.push(flight)
     }
     createFlights(){
-        this.addFlight(new Flight("el-al","TLV - USA",111,10,120,200))
-        this.addFlight(new Flight ("arkia", "USA - TLV", 222,300,100,300))
-        this.addFlight(new Flight("WIZZ", "TLV - MON", 999, 100, 400,1000))
+        const flight1 = new Flight("el-al","TLV - USA",111,10,120,200)
+        const flight2 =new Flight ("arkia", "USA - TLV", 222,300,100,300)
+        const flight3 = new Flight("WIZZ", "TLV - MON", 999, 100, 400,1000)
+        if(flight1.validateFlight().validate.value){
+            this.addFlight(flight1)
+        }
+        else{
+            return flight1.validateFlight()
+        }
+        if(flight2.validateFlight().validate.value){
+            this.addFlight(flight2)
+        }
+        else{
+            return flight2.validateFlight()
+        }
+        if(flight3.validateFlight().validate.value){
+            this.addFlight(flight3)
+        }
+         else{
+            return flight2.validateFlight()
+        }
+    }
+    
+    addTickets(){
+        for (let flight of this.#allFlights){
+            flight.createTicket()
+        }
+    }
+    buyTicket(flightNumber, ticketType, passenger){
+        this.#allFlights.forEach(flight => {
+            if (flight.flightNumber === flightNumber){
+                for (let ticket of flight.ticketsList){
+                    if(!ticket.ownername && ticket instanceof ticketType){
+                        passenger.buyTicket(ticket.price)
+                        ticket.getOwnerName(passenger)
+                    }
+                    return "not enough tickets"
+                }
+            }
+        });
     }
 }
 
